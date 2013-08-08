90e1c07ee39de2614acfac25f0fde3c488345468
Benjamin Hase <benjamin.hase@web.de>
2013-08-08 06:39:12 +0200

    add first feature: player moves
    feature is pending now

---
+++ features/player_movement.feature
@@ -0,0 +1,7 @@
+Feature: Player movement
+	As a player, I can take a turn so that I can move around the board.
+
+	Scenario: Standard move
+		Given a player on location 0
+		When the player rolls 7
+		Then the player shall end on location 7
---
+++ features/steps/step_definitions.rb
@@ -0,0 +1,11 @@
+Given /^a player on location (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the player rolls (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the player shall end on location (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
