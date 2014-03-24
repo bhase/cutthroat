bf8def0f413e4bc72f6de846ba1794e5460e4d53
Benjamin Hase <benjamin.hase@web.de>
2014-03-24 07:04:18 +0100

    remove record_rights from test

--- test/unit/cutthroat/test_game_locations.rb
+++ test/unit/cutthroat/test_game_locations.rb
@@ -8,6 +8,14 @@ class TestGameLocations < Test::Unit::TestCase
   def setup
     @game = Cutthroat::Game.new
     @player = Cutthroat::Player.new
+    callouts = mock
+    # used to remove record_rights from test
+    # now using trigger_action. This may be
+    # a little bit indirect, but uses the public
+    # API. Another possibility would be to
+    # use instance_variable_set
+    callouts.stubs(:buy_property?).returns(true)
+    @player.register_callouts(callouts)
   end
 
   def test_find_locations_owned_by_none_owned
@@ -16,15 +24,15 @@ class TestGameLocations < Test::Unit::TestCase
 
   def test_find_locations_owned_by_one_owned
     location = @game.find_location(5)
-    location.record_rights(@player)
+    location.trigger_action(@player)
     assert_equal([location], @game.find_locations_owned_by(@player))
   end
 
   def test_find_locations_owned_by_two_owned
     l1 = @game.find_location(1)
-    l1.record_rights(@player)
+    l1.trigger_action(@player)
     l2 = @game.find_location(14)
-    l2.record_rights(@player)
+    l2.trigger_action(@player)
     assert_equal([l1, l2], @game.find_locations_owned_by(@player))
   end
 
