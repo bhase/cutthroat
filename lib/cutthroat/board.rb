7b3886644cbc6d158aa050e5af1831ec570a2b0a
Benjamin Hase <benjamin.hase@web.de>
2015-02-24 16:57:40 +0100

    complete default board a little bit more

--- lib/cutthroat/board.rb
+++ lib/cutthroat/board.rb
@@ -51,11 +51,14 @@ Cutthroat::DEFAULT_BOARD = <<ENDOFBOARD
     name: Mediterranean Avenue
     rent: [2, 10, 30, 90, 160, 250]
     land_price: 60
+    house_price: 20
     group: street1
 2:
     name: Community Chest
 3:
     name: Baltic Avenue
+    rent: [4, 20, 60, 180, 320, 450]
+    land_price: 60
     house_price: 50
     group: street1
 4:
@@ -70,6 +73,7 @@ Cutthroat::DEFAULT_BOARD = <<ENDOFBOARD
     name: Oriental Avenue
     rent: [6, 30, 90, 270, 400, 550]
     land_price: 100
+    house_price: 50
     group: street2
 7:
     name: Chance
@@ -77,51 +81,84 @@ Cutthroat::DEFAULT_BOARD = <<ENDOFBOARD
 8:
     name: Vermont Avenue
     rent: [6, 30, 90, 270, 400, 550]
+    land_price: 100
     house_price: 50
     group: street2
 9:
     name: Connecticut Avenue
+    rent: [8, 40, 100, 300, 450, 600]
+    land_price: 120
+    house_price: 50
     group: street2
 10:
     name: Just Visiting
     type: jail
 11:
+    name: St. Charles Place
+    rent: [10, 50, 150, 450, 625, 750]
+    land_price: 140
+    house_price: 100
+    group: street3
 12:
     name: Electric Company
     group: utility
 13:
     name: States Avenue
-    group: street3
+    rent: [10, 50, 150, 450, 625, 750]
     land_price: 140
     house_price: 100
+    group: street3
 14:
     name: Virginia Avenue
+    rent: [12, 60, 180, 500, 700, 900]
     land_price: 160
+    house_price: 100
+    group: street3
 15:
     name: Pennsylvania Railroad
     rent: 25
     group: railroad
 16:
     name: St. James Place
-17:
-18:
-    name: Tennessee Avenue
+    rent: [14, 70, 200, 550, 750, 950]
     land_price: 180
     house_price: 100
+    group: street4
+17:
+    name: Community Chest
+18:
+    name: Tennessee Avenue
+    rent: [14, 70, 200, 550, 750, 950]
+    land_price: 180
+    house_price: 100
+    group: street4
 19:
     name: New York Avenue
+    rent: [16, 80, 220, 600, 800, 1000]
+    land_price: 200
     house_price: 100
+    group: street4
 20:
     name: Free Parking
 21:
     name: Kentucky Avenue
+    rent: [18, 90, 250, 700, 875, 1050]
     land_price: 220
     house_price: 150
+    group: street5
 22:
+    name: Chance
 23:
+    name: Indiana Avenue
+    rent: [18, 90, 250, 700, 875, 1050]
+    land_price: 220
+    house_price: 150
+    group: street5
 24:
     name: Illinois Avenue
     rent: [20, 100, 300, 750, 925, 1100]
+    land_price: 240
+    house_price: 150
     group: street5
 25:
     name: B&O RR
@@ -129,20 +166,22 @@ Cutthroat::DEFAULT_BOARD = <<ENDOFBOARD
     group: railroad
 26:
     name: Atlantic Avenue
+    rent: [22, 110, 330, 800, 975, 1150]
     land_price: 260
     house_price: 150
     group: street6
 27:
     name: Ventnor Avenue
+    rent: [22, 110, 330, 800, 975, 1150]
     land_price: 260
     house_price: 150
-    rent: [22, 110, 330, 800, 975, 1150]
     group: street6
 28:
     name: Water Works
     group: utility
 29:
     name: Marvin Gardens
+    rent: [24, 120, 360, 850, 1025, 1200]
     land_price: 280
     house_price: 150
     group: street6
@@ -152,21 +191,33 @@ Cutthroat::DEFAULT_BOARD = <<ENDOFBOARD
 31:
     name: Pacific Avenue
     rent: [26, 130, 390, 900, 1100, 1275]
+    land_price: 300
+    house_price: 200
     group: street7
 32:
     name: North Carolina Avenue
+    rent: [26, 130, 390, 900, 1100, 1275]
+    land_price: 300
+    house_price: 200
+    group: street7
 33:
+    name: Community Chest
 34:
     name: Pennsylvania Avenue
+    rent: [28, 150, 450, 1000, 1200, 1400]
+    land_price: 320
+    house_price: 200
 35:
     name: Short Line
     rent: 25
     group: railroad
 36:
+    name: Chance
 37:
     name: Park Place
     rent: [35, 175, 500, 1100, 1300, 1500]
     land_price: 350
+    house_price: 200
     group: street8
 38:
     name: Luxury Tax
@@ -174,6 +225,8 @@ Cutthroat::DEFAULT_BOARD = <<ENDOFBOARD
 39:
     name: Boardwalk
     rent: [50, 200, 600, 1400, 1700, 2000]
+    land_price: 400
+    house_price: 200
     group: street8
 ENDOFBOARD
 
